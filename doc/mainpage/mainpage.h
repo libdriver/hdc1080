13dce8e0395d543e7defe7f70a04f440ac92e5f0
LibDriver <704839379@qq.com>
2024-02-11 21:30:45 +0800

    docs: update docs

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,6 +1,6 @@
 /**
  * @mainpage  LibDriver HDC1080
- * @brief     libdriver hdc1080 is a full function driver of hdc1080 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver HDC1080 is a full function driver of HDC1080 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
